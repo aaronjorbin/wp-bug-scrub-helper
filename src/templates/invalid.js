1885c08d2be628ec59cb2f47ca1b43daa3127020
Aaron Jorbin <aaronjorbin@users.noreply.github.com>
2024-01-08 14:10:56 -0400

    Yes, @ them bro

--- src/templates/invalid.js
+++ src/templates/invalid.js
@@ -3,7 +3,7 @@ const { closeTicket } = require( '../helpers/trac_html' );
 module.exports = ( data ) => {
 	closeTicket( 'invalid' );
 	const { author } = data;
-	return `Hi ${ author }, I'm sorry to hear you are having difficulty with your site, however Trac is used for the development of the WordPress software, not for assistance with individual sites or plugins.
+	return `Hi @${ author }, I'm sorry to hear you are having difficulty with your site, however Trac is used for the development of the WordPress software, not for assistance with individual sites or plugins.
 
 I would suggest reaching out to the [https://wordpress.org/support/forums/ WordPress Support Forums] for further assistance. There are volunteers there who can hopefully help you.
 
